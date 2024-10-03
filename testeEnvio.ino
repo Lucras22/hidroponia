9d25230ecd2541dc89456ca58e22330405176f56
Dev. Lucas Galindo <146115927+Lucras22@users.noreply.github.com>
2024-10-03 14:17:58 -0300

    Update testeEnvio.ino

--- testeEnvio.ino
+++ testeEnvio.ino
@@ -11,7 +11,7 @@ const char* ssid = "IFCE_DISCENTES";  // Mantenha o SSID que você deseja
 const char* password = "ifce@bvg";
 
 // Defina a URL do servidor
-String url = "https://devicesserver.onrender.com/api/devices/666840141508522b72de6983";
+String url = "https://devicesserver.onrender.com/api/devices/666840141508522b72de6983"; // Substitua por seu endpoint PUT
 
 // Configurações para o NTP (Time)
 WiFiUDP ntpUDP;
@@ -30,44 +30,10 @@ NTPClient timeClient(ntpUDP, "pool.ntp.org", -3 * 3600, 60000); // UTC-3 para o 
 
 int waterFlux = 0;
 
-// Credenciais do Telegram
-const String botToken = "7330394567:AAFF6ArePK4t3QbCLtZMHgFFEGsyyWoJSVo";
-const String chatId = "7003158288";
-
 // Inicializando sensores
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
 DHT dht(DHT_PIN, DHT22);
-
-// Função para enviar mensagem para o Telegram via POST
-void sendMessage(String message) {
-    if (WiFi.status() == WL_CONNECTED) {
-        HTTPClient http;
-        String url = "https://api.telegram.org/bot" + botToken + "/sendMessage";
-
-        http.begin(url);
-        http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-        // Montando os parâmetros da requisição POST
-        String postData = "chat_id=" + chatId + "&text=" + message;
-
-        // Enviando a requisição
-        int httpResponseCode = http.POST(postData);
-
-        if (httpResponseCode > 0) {
-            String response = http.getString();
-            Serial.println("Código de resposta: " + String(httpResponseCode));
-            Serial.println("Resposta: " + response);
-        } else {
-            Serial.print("Erro na requisição: ");
-            Serial.println(httpResponseCode);
-        }
-
-        http.end();
-    } else {
-        Serial.println("WiFi desconectado");
-    }
-}
 
 void setup() {
     Serial.begin(115200);
@@ -111,13 +77,17 @@ void loop() {
     String Date = String(day) + "/" + String(month) + "/" + String(year);
     String Time = String(hour) + ":" + String(minute) + ":" + String(second);
 
-    String message = "Monitoramento Hidropônico\n\n";
-    
+    // Geração da string JSON para enviar
+    String json = "{";
+    json += "\"espStatus\": true,";
+    json += "\"measures\": [";
+    json += "{";
+
     // Leitura da temperatura da água (DS18B20)
     sensors.requestTemperatures();
     float waterTemperature = sensors.getTempCByIndex(0);
-    message += "Temperatura da água: " + String(waterTemperature) + " °C\n";
-    
+    json += "\"waterTemperature\": " + String(waterTemperature) + ",";
+
     // Leitura do nível de água (HC-SR04)
     digitalWrite(TRIG_PIN, LOW);
     delayMicroseconds(2);
@@ -126,16 +96,27 @@ void loop() {
     digitalWrite(TRIG_PIN, LOW);
     long duration = pulseIn(ECHO_PIN, HIGH);
     float containerLevel = (duration * 0.0343) / 2;
-    message += "Nível de água: " + String(containerLevel) + " cm\n";
+    json += "\"containerLevel\": " + String(containerLevel) + ",";
 
-    // Condição do nível de água (baixo, médio, alto)
-    if (containerLevel > 23.0) {
-        message += "Status do nível de água: Baixo\n";
-    } else if (containerLevel <= 23.0 && containerLevel > 11.5) {
-        message += "Status do nível de água: Médio\n";
+    // Leitura do sensor DHT22
+    float humidity = dht.readHumidity();
+    float temperature = dht.readTemperature();
+    if (isnan(humidity) || isnan(temperature)) {
+        json += "\"humidity\": null,";
+        json += "\"temperature\": null,";
     } else {
-        message += "Status do nível de água: Alto\n";
+        json += "\"humidity\": " + String(humidity) + ",";
+        json += "\"temperature\": " + String(temperature) + ",";
     }
+
+    // Leitura do sensor de condutividade da água (TDS)
+    int tdsValue = analogRead(TDS_PIN);
+    float conductivity = tdsValue * 2;
+    json += "\"conductivity\": " + String(conductivity) + ",";
+
+    // Leitura do sensor LDR (luminosidade)
+    int luminosity = analogRead(LDR_PIN);
+    json += "\"luminosity\": " + String(luminosity) + ",";
 
     // Leitura do índice UV
     int leitura_porta = analogRead(UV_PIN);
@@ -168,55 +149,6 @@ void loop() {
         uv = 11;
     }
 
-    message += "Índice UV: " + String(uv) + " V\n";
-
-    // Leitura do sensor DHT22
-    float humidity = dht.readHumidity();
-    float temperature = dht.readTemperature();
-    if (isnan(humidity) || isnan(temperature)) {
-        message += "Falha ao ler do sensor DHT!\n";
-    } else {
-        message += "Umidade do AR: " + String(humidity) + " %\n";
-        message += "Temperatura do AR: " + String(temperature) + " °C\n";
-    }
-
-    // Leitura do sensor de condutividade da água (TDS)
-    int tdsValue = analogRead(TDS_PIN);
-    float conductivity = tdsValue * 2;
-    message += "Valor PPM: " + String(tdsValue) + "\n";
-    message += "Valor ECC: " + String(conductivity) + " \n";
-
-    // Leitura do sensor LDR (luminosidade)
-    int luminosity = analogRead(LDR_PIN);
-    message += "Intensidade Luminosa: " + String(luminosity) + "\n";
-
-    // Condição do período (dia ou noite)
-    if (luminosity < 2000) {
-        message += "Período do dia: Dia\n";
-    } else {
-        message += "Período do dia: Noite\n";
-    }
-
-    // Fluxo de água utilizando o estado do botão
-    waterFlux = digitalRead(BUTTON_PIN);
-    if (waterFlux == LOW) { 
-        message += "Há fluxo de água\n";
-    } else {
-        message += "Não há fluxo de água\n";
-    }
-
-    // Geração da string JSON para enviar
-    String json = "{";
-    json += "\"espStatus\": true,";
-    json += "\"measures\": [";
-    json += "{";
-    json += "\"temperature\": " + String(temperature) + ",";
-    json += "\"waterTemperature\": " + String(waterTemperature) + ",";
-    json += "\"waterFlux\": " + String(waterFlux) + ",";
-    json += "\"containerLevel\": " + String(containerLevel) + ",";
-    json += "\"conductivity\": " + String(conductivity) + ",";
-    json += "\"humidity\": " + String(humidity) + ",";
-    json += "\"luminosity\": " + String(luminosity) + ",";
     json += "\"uv\": " + String(uv) + ",";
     json += "\"date\": \"" + Date + "\",";
     json += "\"time\": \"" + Time + "\"";
@@ -230,7 +162,7 @@ void loop() {
         http.begin(url);
         http.addHeader("Content-Type", "application/json");
         
-        int httpResponseCode = http.POST(json);
+        int httpResponseCode = http.PUT(json); // Alterado de POST para PUT
 
         if (httpResponseCode > 0) {
             String response = http.getString();
@@ -244,8 +176,5 @@ void loop() {
         Serial.println("WiFi desconectado");
     }
 
-    // Enviar mensagem para o Telegram
-    sendMessage(message);
-    
     delay(10000); // Delay de 10 segundos antes da próxima leitura
 }
