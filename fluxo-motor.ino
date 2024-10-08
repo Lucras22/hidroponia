e54a792839372e903a364bf8a84c41d53ffde0d3
Dev. Lucas Galindo <146115927+Lucras22@users.noreply.github.com>
2024-10-08 16:12:39 -0300

    Update fluxo-motor.ino

--- fluxo-motor.ino
+++ fluxo-motor.ino
@@ -1,33 +1,27 @@
-//https://www.arduitronics.com/product/2337/water-flow-sensor-1-30l-min-%E0%B9%81%E0%B8%A3%E0%B8%87%
-//E0%B8%94%E0%B8%B1%E0%B8%99%E0%B9%84%
-//E0%B8%A1%E0%B9%88%E0%B9%80%E0%B8%81%E0%B8%B4%E0%B8%99
-//-1-75mpa-%E0%B8%82%E0%B8%99%E0%B8%B2%E0%B8%94%E0%B8%97%E0%B9%88%E0%B8%AD
-//-1-4%E2%80%9D-yf-s201c-%E0%B9%81%E0%B8%9A%E0%B8%9A%E0%B9%83%E0%B8%AA
+// Pino onde o fio amarelo (sinal) do sensor está conectado
+const int waterFluxPin = 13;
 
+// Variável para armazenar o estado atual do sensor
+int waterFlux = LOW;
 
-volatile int NbTopsFan; //measuring the rising edges of the signal
-int Calc;
-int hallsensor = 2; //The pin location of the sensor
+void setup() {
+  // Inicializa o monitor serial para visualização dos dados
+  Serial.begin(115200);
 
-void rpm() //This is the function that the interupt calls
-{
-NbTopsFan++; //This function measures the rising and falling edge of the hall effect sensors signal
+  // Configura o pino do sensor de fluxo como entrada
+  pinMode(waterFluxPin, INPUT);
 }
 
-void setup()
-{
- pinMode(hallsensor, INPUT); //initializes digital pin 2 as an input
-Serial.begin(9600); //This is the setup function where the serial port is initialised,
-attachInterrupt(0, rpm, RISING); //and the interrupt is attached
-}
+void loop() {
+  // Lê o estado do pino do sensor de fluxo
+  waterFlux = digitalRead(waterFluxPin);
 
-void loop()
-{
-NbTopsFan = 0; //Set NbTops to 0 ready for calculations
-sei(); //Enables interrupts
-delay(1000); //Wait 1 second
-cli(); //Disable interrupts
-Calc = (NbTopsFan * 60 / 7.5); //(Pulse frequency x 60) / 7.5Q, = flow rate in L/hour
-Serial.print(Calc, DEC); //Prints the number calculated above
-Serial.print(" L/hour\r\n"); //Prints "L/hour" and returns a new line
-}   
+  if (waterFlux == HIGH) {
+    Serial.println("Não Há fluxo de água");
+  } else {
+    Serial.println("há fluxo de água");
+  }
+
+  // Pequeno atraso para não sobrecarregar o monitor serial
+  delay(500);
+}
